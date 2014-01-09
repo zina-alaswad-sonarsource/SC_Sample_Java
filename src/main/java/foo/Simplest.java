44e0c5b4fc18a006a405d3f3d489896245fab3d1
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-01-09 16:48:59 +0100

    Add 1 more issue

--- src/main/java/foo/Simplest.java
+++ src/main/java/foo/Simplest.java
@@ -8,6 +8,9 @@ public class Simplest {
     public static int add(int a, int b) {
     	// introduce a variable that is not needed - just to get a violation
     	int result = a + b;
+
+    	System.out.println("");
+
         return result;
     }
 
