1379c8278cd6016f3e215624b1496c45df017ddb
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-06-09 15:56:04 +0200

    Add other tests in error & failure

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ fake-project-for-tests
 ======================
 
 Simple project used for making tests on SonarQube with Scm Activity Plugin.
+
+Just run "./runAnalysis.sh" to get the project analyzed, and "./buildHistoryOfAnalyses.sh" to get some historical data (interesting with SCM Activity and DevCockpit plugins)
--- src/test/java/foo/SimplestTest.java
+++ src/test/java/foo/SimplestTest.java
@@ -9,4 +9,17 @@ public class SimplestTest {
   public void testAdd() throws Exception {
   	assertEquals(Simplest.add(4, 5), 9);
   }
+
+  @Test
+  public void testAdd_WhichFails() throws Exception {
+  	assertEquals(Simplest.add(4, 5), 2);
+  }
+
+  @Test
+  public void testAdd_InError() throws Exception {
+  	assertEquals(Simplest.add(4, 5), 9);
+  	String o = null;
+  	o.toString();
+  }
+
 }
