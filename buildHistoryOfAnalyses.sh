17b35159480d43f65d066b605673ad2cd6de5a99
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-06-23 17:14:24 +0200

    Update history script

--- buildHistoryOfAnalyses.sh
+++ buildHistoryOfAnalyses.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 
+analyze() {
+  mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=true $*
+  mvn sonar:sonar $*
+} 
+
 git checkout 37acee5
-./runAnalysis.sh -Dsonar.projectDate=2013-09-22 $*
+analyze -Dsonar.projectDate=2013-09-22 $*
 sonar-runner devcockpit -Dsonar.projectDate=2014-01-22 $*
 # => 2013-09-22
 
 git checkout 322e4e8
-./runAnalysis.sh -Dsonar.projectDate=2013-12-22 $*
+analyze -Dsonar.projectDate=2013-12-22 $*
 sonar-runner devcockpit -Dsonar.projectDate=2013-12-22 $*
 # => 2013-12-22
 
 git checkout 44e0c5b
-./runAnalysis.sh -Dsonar.projectDate=2014-01-22 $*
+analyze -Dsonar.projectDate=2014-01-22 $*
 sonar-runner devcockpit -Dsonar.projectDate=2014-01-22 $*
 # => 2014-01-22
 
 git checkout aaf2cb9
-./runAnalysis.sh -Dsonar.projectDate=2014-02-01 $*
+analyze -Dsonar.projectDate=2014-02-01 $*
 sonar-runner devcockpit -Dsonar.projectDate=2014-02-01 $*
 # => 2014-02-01
 
 git checkout c956a65
-./runAnalysis.sh -Dsonar.projectDate=2014-02-10 $*
+analyze -Dsonar.projectDate=2014-02-10 $*
 sonar-runner devcockpit -Dsonar.projectDate=2014-02-10 $*
 # => 2014-02-10
 
