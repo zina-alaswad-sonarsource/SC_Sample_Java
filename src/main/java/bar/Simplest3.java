f22337479b19901f85ec6dc2f1bbd41ba6c7a96c
James Gosling <james.gosling@java.world>
2015-06-09 09:35:24 +0200

    Adding some buggy code

--- src/main/java/bar/Simplest3.java
+++ src/main/java/bar/Simplest3.java
@@ -3,8 +3,14 @@ package bar;
 public class Simplest3 {
 	private Simplest2 s;
 
+	public String name;
+
 	private void foo() {
 		int i = 0;
 		int b = 5;
+
+		Object o = null;
+		name = o.toString();
+		System.out.println("=> " + o);
 	}
 }
