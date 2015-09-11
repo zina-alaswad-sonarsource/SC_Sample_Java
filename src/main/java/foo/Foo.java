aa0428b96f0f157c9d864d156c69b0a269248c75
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2015-09-11 18:24:06 +0200

    Commit some changes

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -8,6 +8,7 @@ import bar.Simplest;
 public class Foo {
 
     public static int times(int a, int b) {
+    	// Use Simplest to introduce a dependency
     	int i = Simplest.add(5, 6);
         return a * b;  
     }
